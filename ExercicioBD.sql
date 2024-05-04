29323970813e91a017d05b6937c8d5180c1a8c6a
Matheus Muniz <72587336+Munizzs@users.noreply.github.com>
2024-05-04 16:18:27 -0300

    Add files via upload
    Exercícios Resolvido Banco de Dados

---
+++ ExercicioBD.sql
@@ -0,0 +1,109 @@
+-- 1
+
+SELECT 
+	Nome,
+	Ano	
+FROM Filmes
+
+-- 2
+
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+ORDER BY Ano
+
+-- 3
+
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Id = 28
+
+-- 4
+
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano = 1997
+
+-- 5
+
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Ano > 2000
+
+-- 6
+
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+-- 7
+
+SELECT
+	Ano,
+	Count(Ano) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+-- 8
+
+SELECT
+	*
+FROM Atores
+WHERE Genero = 'M'
+
+-- 9
+
+SELECT
+	*
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+-- 10
+
+SELECT
+	Filmes.Nome,
+	Generos.Genero 
+FROM FilmesGenero
+INNER JOIN Filmes ON Filmes.Id = FilmesGenero.IdFilme 
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+
+
+-- 11
+
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM FilmesGenero
+INNER JOIN Filmes ON Filmes.Id = FilmesGenero.IdFilme 
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE Genero = 'Mistério'
+
+-- 12
+
+SELECT
+	Filmes.Nome,
+	Atores.PrimeiroNome,
+	Atores.UltimoNome,
+	Papel
+FROM ElencoFilme
+INNER JOIN Filmes ON Filmes.Id = ElencoFilme.IdFilme 
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
+
+
